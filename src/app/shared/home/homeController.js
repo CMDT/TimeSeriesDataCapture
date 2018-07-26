3685c6f7cda22b743ea0f719a550de04db1e8664
unknown <ybandar1902@gmail.com>
2018-07-26 09:04:48 +0100

    Search Results Styling and Angular Materials added

--- src/app/shared/home/homeController.js
+++ src/app/shared/home/homeController.js
@@ -1,6 +1,4 @@
-
-
-app.controller('homeController', ['$scope', '$log','$filter', 'authenticationService', 'oneDriveAuthenticationService', 'searchService', function ($scope, $log,$filter, authenticationService, oneDriveAuthenticationService, searchService) {
+app.controller('homeController', ['$scope', '$log', '$filter', 'authenticationService', 'oneDriveAuthenticationService', 'searchService', function ($scope, $log, $filter, authenticationService, oneDriveAuthenticationService, searchService) {
 
     $scope.login = function () {
         authenticationService.login();
@@ -15,27 +13,48 @@ app.controller('homeController', ['$scope', '$log','$filter', 'authenticationSer
     $scope.inputChange = function (search) {
         if (search.length > 0) {
             searchService.tagPrediction($filter('lowercase')(search)).then(function (result) {
-            
+
                 $scope.search = result[0].tag;
                 $scope.$apply();
             });
 
-           
+
         }
 
     }
 
-    $scope.searchClick = function(){
-        if($scope.search.length > 0){
-            searchService.searchRequest().then(function(result){
+    $scope.searchClick = function () {
+        if ($scope.search.length > 0) {
+            searchService.searchRequest().then(function (result) {
                 $scope.searchResult = result;
                 $scope.$apply();
             })
         }
     }
 
+    $scope.results = [{
+        id: 3463274,
+        time: 11324,
+        date : '213/213/213',
+    },
+    {
+        id: 3463274,
+        time: 11324,
+        date : '213/213/213',
+    }]
+
+
+
+    $scope.autoTextChange = function(){
+        $log.log('text changed');
+    }
+
+    $scope.autoItemChange = function(item){
+        $log.log($scope.selectedItem);
+    }
+
+    $scope.autoItems = ['tag1','tag2','tag3'];
+
 
 
 }])
-
-
