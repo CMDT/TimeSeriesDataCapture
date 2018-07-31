51f673831aee50bb1e0c7d1de49104cd03b5841e
Yusof Bandar <ybandar1902@gmail.com>
2018-07-31 15:58:47 +0100

    express server added

--- src/server.js
+++ src/server.js
@@ -1,5 +1,27 @@
-var connect = require('connect');
-var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname)).listen(8080, function(){
-    console.log('Server running on 8080...');
+const express = require('express');
+const http = require('http')
+const path = require('path');
+
+const app = express();
+
+
+app.use(express.static(path.join(__dirname, '')));
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/index.html'));
 });
+
+const port = process.env.PORT || 8080;
+app.set('port', port);
+
+const server = http.createServer(app);
+server.listen(port, () => console.log('running'));
+
+
+
+    
+
+
+
+
+
