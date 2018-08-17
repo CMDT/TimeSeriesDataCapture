f7f21b42c98a0a516c7e804270a17fa00222eb58
Yusof Bandar <ybandar1902@gmail.com>
2018-08-17 15:34:26 +0100

    columns/tags tabs repeated

--- src/app/shared/view/viewController.js
+++ src/app/shared/view/viewController.js
@@ -1,7 +1,7 @@
 app.controller('viewController', ['$scope', '$log', 'timeSeriesGraphService', function ($scope, $log,timeSeriesGraphService) {
 
 
-   
+   $scope.runs = ['run1','run2','run3'];
 
 
 
